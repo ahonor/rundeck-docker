f4bd40fcb8d8a21dacc09f642e47726def660ade
Alex Honor <alexhonor@yahoo.com>
2015-06-30 14:50:32 -0700

    added step plugin for docker run

---
+++ plugins/docker-command/README.md
@@ -0,0 +1,12 @@
+
+
+# Example configuration
+
+
+project.plugin.WorkflowStep.docker-command-step.docker_cert_path=/Users/alexh/.boot2docker/certs/boot2docker-vm
+project.plugin.WorkflowStep.docker-command-step.docker_host=tcp\://192.168.59.103\:2376
+project.plugin.WorkflowStep.docker-command-step.docker_tls_verify=true
+#
+project.plugin.WorkflowStep.docker-run-step.docker_cert_path=/Users/alexh/.boot2docker/certs/boot2docker-vm
+project.plugin.WorkflowStep.docker-run-step.docker_host=tcp\://192.168.59.103\:2376
+project.plugin.WorkflowStep.docker-run-step.docker_tls_verify=true
--- plugins/docker-nodes/src/contents/nodes.sh
+++ plugins/docker-nodes/src/contents/nodes.sh
@@ -13,19 +13,28 @@ export DOCKER_CERT_PATH=$RD_CONFIG_DOCKER_CERT_PATH
 echo "<project>"
 docker ps|grep -v 'CONTAINER'|cut -f1 -d" "| while read container
 do
+        Created=$(docker inspect -f '{{.Created}}' $container)
+        containerId=$(docker inspect -f '{{.Id}}' $container)
+
         hostname=$(docker inspect -f '{{.Config.Hostname}}' $container)
         username=$(docker inspect -f '{{.Config.User}}' $container)
         exposed_ports=$(docker inspect -f '{{.Config.ExposedPorts}}' $container)
         image=$(docker inspect -f '{{.Config.Image}}' $container)
-        containerId=$(docker inspect -f '{{.Id}}' $container)
-        status=$(docker inspect -f '{{.State.Running}}' $container)
+        WorkingDir=$(docker inspect -f '{{.Config.WorkingDir}}' $container)
         name=$(docker inspect -f '{{.Name}}' $container)
+
+        Running=$(docker inspect -f '{{.State.Running}}' $container)
+        Paused=$(docker inspect -f '{{.State.Paused}}' $container)
+
         IPAddress=$(docker inspect -f '{{.NetworkSettings.IPAddress}}' $container)
-        Created=$(docker inspect -f '{{.Created}}' $container)
+
 
 cat <<EOF
-<node  name='$container' containerId='$containerId' username='$username' exposed_ports='$exposed_ports' tags='container,$image' hostname='$hostname' >
-<attribute name="Running" value='$status'/>
+<node  name='$container' description='$name' containerId='$containerId' username='$username' tags='container,$image' hostname='$hostname' >
+<attribute name="ExposedPorts" value='$exposed_ports'/>
+<attribute name="WorkingDir" value='$WorkingDir'/>
+<attribute name="Running" value='$Running'/>
+<attribute name="Paused" value='$Paused'/>
 <attribute name="Name" value='$name'/>
 <attribute name="Image" value='$image'/>
 <attribute name="Created" value='$Created'/>
